082d3a8800484b2777706c1120f388019faea6a4
henrynguyen6677 <henrynguyen6677@gmail.com>
2024-08-31 08:07:15 +0700

    Add unit test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,14 @@
-import CustomScrollbar from '.'
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { CustomScrollbar } from './index'
 
-describe('CustomScrollbar', () => {
-  it('is truthy', () => {
-    expect(CustomScrollbar).toBeTruthy()
-  })
+test('handles vertical scroll', () => {
+  const { container } = render(
+    <CustomScrollbar height={300}>
+      <div style={{ height: '1000px' }}>Scrollable Content</div>
+    </CustomScrollbar>
+  )
+  const scrollContainer = container.querySelector('.scrollContainer')
+  fireEvent.scroll(scrollContainer, { target: { scrollTop: 100 } })
+  expect(scrollContainer.scrollTop).toBe(100)
 })
