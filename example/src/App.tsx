6c41f982c3fc27565a22f368f390e2593a5a40ef
henrynguyen6677 <henrynguyen6677@gmail.com>
2024-08-31 11:08:09 +0700

    Update sample code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react'
-
-import { ExampleComponent } from 'react-mini-scrollbar'
 import 'react-mini-scrollbar/dist/index.css'
+import { CustomScrollbar } from 'react-mini-scrollbar'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div
+      style={{
+        position: 'relative',
+        top: '2rem',
+        margin: 'auto',
+        height: '300px',
+        width: '300px'
+      }}
+    >
+      <CustomScrollbar
+        right={1}
+        thumbColor='#f00'
+        trackColor='#0f0'
+        thumbWidth={10}
+        trackWidth={10}
+        height={300}
+        isShowTrack={true}
+        className='custom-scrollbar'
+      >
+        <div
+          style={{
+            height: '500px',
+            width: '100%',
+            background: '#ccc'
+          }}
+        ></div>
+      </CustomScrollbar>
+    </div>
+  )
 }
 
 export default App
