2777b90baf1b0e0e72c9b3bffe2ba9e6078301d2
henrynguyen6677 <henrynguyen6677@gmail.com>
2024-09-20 17:33:43 +0700

    Updates

---
+++ precache-manifest.ffbb5d2476568260ab9a250c4024c45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57958d5ad1f6bb8336d2a6073e8c0ea0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "322b645379b2291b7d04",
+    "url": "./static/css/main.8e04f1f7.chunk.css"
+  },
+  {
+    "revision": "4a688eebf8ca2d1b64b0",
+    "url": "./static/js/2.6b2fb3f2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.6b2fb3f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "322b645379b2291b7d04",
+    "url": "./static/js/main.27b9144b.chunk.js"
+  },
+  {
+    "revision": "1d09034b057217ac07a5",
+    "url": "./static/js/runtime-main.f2cdb739.js"
+  }
+]);
